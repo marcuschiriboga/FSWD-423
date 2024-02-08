c3caf9062e6c6116c40a6ded352a2c7c46192210
marcuschiriboga <marcus.chiriboga@gmail.com>
2024-02-08 11:48:23 -0500

    week3 wednesday

--- week_3/react/src/App.jsx
+++ week_3/react/src/App.jsx
@@ -1,39 +1,85 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { forwardLines, defenseLines, goalies } from "./teamRoster";
-import { wholeTeam } from "./teamRoster";
-import { PlayerCard } from "./components/PlayerCard";
 import "./App.css";
+import { PlayerCard } from "./components/PlayerCard";
+import { defenseLines, forwardLines, goalies } from "./teamRoster";
 
 function App() {
-   {/* build cards for the hockey team  √ */}
-   {/* abstract cards to their own components:
-       build a flexible and reusable component*/}
-   {/* pass props and conditionally render the components */}
+  let [forwardLineNumber, setForwardLineNumber] = useState(0);
 
+  const [oneForwardLine, setOneForwardLine] = useState(
+    forwardLines[forwardLineNumber]
+  );
+  const [oneDefenseLine, setOneDefenseLine] = useState(defenseLines[0]);
+
+  {
+    /* build cards for the hockey team  √*/
+  }
+  {
+    /* abstract cards to their own components:
+       build a flexible and reusable component*/
+  }
+  {
+    /* pass props and conditionally render the components */
+  }
+
+  //  day 2 each position should show one line at a time.
+
+  // we will need something to select which one (function)
+  // if we have time we can create a form to update state.
+  // (refactor to use useState)?
+  // 
+
+  let updateForwardLine = () => {
+    if (oneForwardLine.line < forwardLines.length - 1) {
+      setForwardLineNumber(forwardLineNumber + 1);
+    } else {
+      setForwardLineNumber(0);
+    }
+    setOneForwardLine(forwardLines[forwardLineNumber]);
+    console.log(forwardLines.length, forwardLineNumber);
+  };
   return (
     <>
       <h1>Hockey Roster</h1>
-
-      <h3>Offense</h3>
-      <div>
+      <div className="forwards" style={{ outline: "4px solid red" }}>
+        <h3>Offense line: {forwardLineNumber + 1}</h3>
+        <button onClick={updateForwardLine}>Next Line</button>
         {/* this is one forward line. we map and make a structure that shows all 3 lines below */}
-        {forwardLines[0].players.map(player => {
+        {oneForwardLine.players.map(player => {
           return <PlayerCard player={player} />;
         })}
       </div>
-      <h3>Defense hoo haha</h3>
-      <div>
-        {defenseLines.map(defenseLine => {
-          let lines = defenseLine.players.map(player => {
-            return <PlayerCard player={player}/>;
-          });
-          return <><h3>Line {defenseLine.line}</h3><div>{lines}</div></>;
+      <div className="defense" style={{ outline: "4px solid red" }}>
+        <h3>Defense hoo haha</h3>
+        {/* loop over defenseLines.length and create buttons dynamically and 
+      pass the incrementor to each button to update state  */}
+        <button
+          onClick={() => {
+            setOneDefenseLine(defenseLines[0]);
+          }}
+        >
+          line 1
+        </button>
+        <button
+          onClick={() => {
+            setOneDefenseLine(defenseLines[1]);
+          }}
+        >
+          line 2
+        </button>
+        <button
+          onClick={() => {
+            setOneDefenseLine(defenseLines[2]);
+          }}
+        >
+          line 3
+        </button>
+        {oneDefenseLine.players.map(player => {
+          return <PlayerCard player={player} />;
         })}
       </div>
-      <h3>Goalies</h3>
-      <div>
+      <div className="goalies" style={{ outline: "4px solid red" }}>
+        <h3>Goalies</h3>
         {goalies.map(player => {
           return <PlayerCard player={player} />;
         })}
